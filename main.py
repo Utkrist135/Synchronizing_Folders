d9d0e542f759cee957b4700577d5e8cea9b2d78b
Utkrisy <upretyutkrist@gmail.com>
2023-01-11 14:39:26 +0100

    Removed Some bugs and completed the project

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import shutil as sh
 import os
 import filecmp
+import logging
+import time
 
 
 
-    
+#Create and configure logger 
+logging.basicConfig(filename="Logfile.log", 
+					format='%(asctime)s %(message)s', 
+					filemode='w') 
+
+
+logger=logging.getLogger() 
+
+# Set the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG) 
+
+
+
 
 def are_dir_trees_equal(dir1, dir2):
     """
     Compare two directories recursively. Files in each directory are
-    assumed to be equal if their names and contents are equal.
+    assumed to be equal if their names and contents are equal. Copy into replica / Delete from Replica
 
     @param dir1: First directory path
     @param dir2: Second directory path
@@ -20,25 +34,43 @@ def are_dir_trees_equal(dir1, dir2):
    """
 
     dirs_cmp = filecmp.dircmp(dir1, dir2)
-    if len(dirs_cmp.left_only)>0 or len(dirs_cmp.right_only)>0 or \
-        len(dirs_cmp.funny_files)>0:  
-        for str in dirs_cmp.right_only:
-            
-            path = os.path.join(dir2,str)
+    if len(dirs_cmp.left_only)>0:
+        print('<<<---Copying the following files/folders into Replica--->>>' )
+        logger.info("<<<---Copying the following files/folders into Replica--->>>")
+        for str1 in dirs_cmp.left_only:
+            path1 = os.path.join(dir1,str1)
+            if os.path.isdir(path1):
+                print("<<<---Copied Folder from Source into Replica--->>>")
+                logger.info("<<<---Copied Folder from Source into Replica--->>>")
+                print(path1)
+                logger.info(path1)    
+            if os.path.isfile(path1):
+                print("<<<<<---------Copied file from Source into Replica--->>>")
+                logger.info("<<<---Copied file from Source into Replica--->>>")
+                print(path1)
+                logger.info(path1)
+           
+    if len(dirs_cmp.right_only)>0:
+        for str2 in dirs_cmp.right_only:
+            path = os.path.join(dir2,str2)
             if os.path.isfile(path):
                 os.remove(path)
-                print("----------------Removed file at-----------")
+                print("<<<---Removed file at--->>>")
+                logger.info(" <<<---Removed file at--->>> ")
                 print(path)
+                logger.info(path)
             if os.path.isdir(path):
                 sh.rmtree(path)
-                print("---------------Removed entire directory tree at-----------")
-                print(path)    
-        return True
-    (_, mismatch, errors) =  filecmp.cmpfiles(
-        dir1, dir2, dirs_cmp.common_files, shallow=False)
-    if len(mismatch)>0 or len(errors)>0:  
-        print("mismatch or errors")          
+                print("<<<---Removed entire directory tree at--->>>")
+                logger.info("<<<---Removed entire directory tree at--->>>")
+                print(path)
+                logger.info(path)  
+        return True           
+    if len(dirs_cmp.funny_files)>0:
+        print('Some files could not be compared!!!!!!')      
+        logger.warning("Some files could not be compared!!!")  
         return False
+    
     for common_dir in dirs_cmp.common_dirs:
         new_dir1 = os.path.join(dir1, common_dir)
         new_dir2 = os.path.join(dir2, common_dir)
@@ -47,42 +79,28 @@ def are_dir_trees_equal(dir1, dir2):
     return True
 
 
+
 src_path = 'Source'
 rep_path = 'Replica'
+check = True
+synchronisation_interval_in_sec = 30 
+while(check):
+    print('<<<--- The Synchronisation interval is '+ str(synchronisation_interval_in_sec) + 'sec')  
+    if are_dir_trees_equal(src_path,rep_path) == True:
+        sh.copytree(src_path, rep_path, symlinks=False, ignore=None, ignore_dangling_symlinks=False, dirs_exist_ok=True)
+        print('<<<---The folders are now synchronised!!--->>>')
+        logger.info("<<<---The folders are now synchronised!!--->>>")
+        
+        print('Press Ctrl+C to exit the program')
+        
+    else:
+        print('!!!!!!!!!!!!ERROR!!!!!!!!!!!!')
+        logger.error("!!!!!!!!!ERROR!!!!!!!!!!!!")
+        
+    time.sleep(synchronisation_interval_in_sec)    
+    
 
 
-sh.copytree(src_path, rep_path, symlinks=False, ignore=None, ignore_dangling_symlinks=False, dirs_exist_ok=True)
-print ('Copied')
-
-
-src_files = set()
-src_dirs = set()
-rep_files = set()
-rep_dirs = set()
-
-for (dir_, _, files) in os.walk(src_path):
-    for file_name in files:
-        src_dirs_temp = os.path.relpath(dir_,src_path)
-        src_files_temp = os.path.join(src_dirs_temp, file_name)
-        src_files.add(src_files_temp)
-        src_dirs.add(src_dirs_temp)
-
-for (dir_, _, files) in os.walk(rep_path):
-    for file_name in files:
-        rep_dirs_temp = os.path.relpath(dir_,rep_path)
-        rep_files_temp = os.path.join(rep_dirs_temp, file_name)
-        rep_files.add(rep_files_temp)
-        rep_dirs.add(rep_dirs_temp)        
-
-
-
-
-
-if (are_dir_trees_equal(src_path, rep_path)):
-
-    print ("The folders are fully synchronized")
-else:
-    print ("Not fully synchronised")
     
    
                     
@@ -90,9 +108,7 @@ else:
         
             
         
-        
-    
-          
+      
                                    
     
         
